6ad9e0b6511a88043632c357c04718b20530fa8c
Kevindrayana <kistimur@connect.hku.hk>
2023-05-30 01:02:58 +0800

    update projects description

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -4,6 +4,7 @@ import MazeSolver from "../../../content/projects/MazeSolver.mdx";
 import PhoneShop from "../../../content/projects/PhoneShop.mdx";
 import BigTwoGame from "../../../content/projects/BigTwoGame.mdx";
 import INotes from "../../../content/projects/iNotes.mdx";
+import TutorManagementSystem from "../../../content/projects/TutorManagementSystem.mdx";
 import ProjectButton from "components/buttons/projectButton";
 
 export default function Project({ params }) {
@@ -11,13 +12,14 @@ export default function Project({ params }) {
     <>
       <div className="font-bold text-3xl font-serif max-w-[650px]"></div>
       <article className="prose dark:prose-invert prose-lg prose-headings:text-green-500">
+        {params.id == "Tutor-Management-System" && <TutorManagementSystem />}
         {params.id == "Maze-Solver" && <MazeSolver />}
-        {params.id == "Phone-Shop" && <PhoneShop/>}
-        {params.id == "Big-Two-Game" && <BigTwoGame/>}
-        {params.id == "iNotes" && <INotes/>}
+        {params.id == "Phone-Shop" && <PhoneShop />}
+        {params.id == "Big-Two-Game" && <BigTwoGame />}
+        {params.id == "iNotes" && <INotes />}
       </article>
       <br />
-      <ProjectButton/>
+      <ProjectButton />
     </>
   );
 }
--- app/projects/projectData.ts
+++ app/projects/projectData.ts
@@ -1,24 +1,29 @@
 const projects = [
-    {
-      title: "Maze-Solver",
-      description:
-        "Implemented Dijkstra's algorithm to create a pathfinding visualizer that allows users to customize and generate random mazes, finding optimal solutions for each",
-    },
-    {
-      title: "iNotes",
-      description:
-        "A full-stack clone of a general note-taking app with features such as user authentication (login/logout), CRUD functionality for posting and deleting notes, and searching",
-    },
-    {
-      title: "Phone-Shop",
-      description:
-        "A fully responsive SPA that simulates the front end of a common e-commerce site. Users may toggle details, add products to a cart, and slide between pages",
-    },
-    {
-      title: "Big-Two-Game",
-      description:
-        "A client-server BigTwo card game with a responsive GUI, allowing up to 4 concurrent clients to play Big Two Card Game and chat with each other",
-    },
-  ];
+  {
+    title: "Tutor-Management-System",
+    description:
+      "Streamlined communication between 100+ tutors and admins by designing and implementing a full-stack Tutor Management web application, replacing conventional email communication"
+  },
+  {
+    title: "Maze-Solver",
+    description:
+      "Implemented Dijkstra's algorithm to create a pathfinding visualizer that allows users to customize and generate random mazes, finding optimal solutions for each",
+  },
+  {
+    title: "iNotes",
+    description:
+      "A full-stack clone of a general note-taking app with features such as user authentication (login/logout), CRUD functionality for posting and deleting notes, and searching",
+  },
+  {
+    title: "Phone-Shop",
+    description:
+      "A fully responsive SPA that simulates the front end of a common e-commerce site. Users may toggle details, add products to a cart, and slide between pages",
+  },
+  {
+    title: "Big-Two-Game",
+    description:
+      "A client-server BigTwo card game with a responsive GUI, allowing up to 4 concurrent clients to play Big Two Card Game and chat with each other",
+  },
+];
 
-  export default projects
+export default projects;
